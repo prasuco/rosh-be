f74e163364f8444fedb411de5709001a6e6be2fc
prabincankod <prabinsubedi2016@gmail.com>
2024-03-26 20:13:32 +0545

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import cors from 'cors'
 import { authRouter } from "./routers/auth.router";
 import { postRouter } from "./routers/post.router";
 
 import authMiddleware from "./middlewares/auth.middleware";
 
 const app = express();
+
 app.use(express.json());
+app.use(cors())
 const port = process.env.PORT || 3333;
 
 app.get("/", (res, req) => {
--- src/routers/post.router.ts
+++ src/routers/post.router.ts
@@ -1,29 +1,23 @@
 import { Router } from "express";
-import { CompleteArticle, articleSchema } from "../../prisma/zod";
-
 import { Prisma } from "./../../node_modules/.prisma/client/index";
 import { prisma } from "../../prisma/prisma";
-import { log } from "console";
 import { StatusCodes } from "http-status-codes";
-import { z } from "zod";
 import { PrismaClientValidationError } from "@prisma/client/runtime/library";
 export const postRouter = Router();
 
 postRouter.post("/", async (req, res) => {
   // TODO: Implement article creation
-  const { content, name, description } = req.body as Prisma.ArticleCreateInput;
+  const { content, name, description} = req.body as Prisma.ArticleCreateInput;
   try {
     const newArticle = await prisma.article.create({
       data: { content, description, name },
     });
     res.status(StatusCodes.CREATED).json({ success: true, data: newArticle });
   } catch (error) {
-    
-    if(error instanceof PrismaClientValidationError){
-
-        res
+    if (error instanceof PrismaClientValidationError) {
+      res
         .status(StatusCodes.INTERNAL_SERVER_ERROR)
         .json({ success: false, message: error.message });
     }
-}
+  }
 });
