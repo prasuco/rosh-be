2f11335eb9c7315babf73ed66b65a3748a00ef76
prabincankod <prabinsubedi2016@gmail.com>
2024-04-03 12:43:35 +0545

    setup a global error handler for now.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import express from "express";
-import cors from 'cors'
+import express, {
+  ErrorRequestHandler,
+  NextFunction,
+  Request,
+  Response,
+} from "express";
+import cors from "cors";
 import { authRouter } from "./routers/auth.router";
 import { postRouter } from "./routers/post.router";
 
@@ -8,7 +13,15 @@ import authMiddleware from "./middlewares/auth.middleware";
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ success: false, message: "Something Broke" });
+};
+
+app.use(globalErrorHandler);
+
+app.use(cors());
 const port = process.env.PORT || 3333;
 
 app.get("/", (res, req) => {
